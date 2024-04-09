32809b327eebeab2de3fb607bc9812042eca8012
Tarun Angrish <angrish.t@northeastern.edu>
2024-04-08 22:43:01 -0400

    Form added

--- frontend/src/components/SellNFT/SellNFT.tsx
+++ frontend/src/components/SellNFT/SellNFT.tsx
@@ -1,10 +1,27 @@
 "use client";
 
 import React, { useState } from "react";
-
-
+import FormField from "./FormField";
+import FormTextArea from "./FormTextArea";
 import { useContract, useContractRead, useAddress } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
+import { deployedContract } from "@/constants/index";
+import { useRouter } from "next/navigation";
+import { pinFileToIPFS, pinJSONToIPFS } from "@/utils/Pinata";
+
+type formFields = {
+    name: string;
+    description: string;
+    price: string;
+    imageUrl: string;
+};
+
+const initialFormState = {
+    name: "",
+    description: "",
+    price: "",
+    imageUrl: "",
+};
 
 const SellNft = () => {
     const address = useAddress();
@@ -66,6 +83,20 @@ const SellNft = () => {
         }
     };
 
+    const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        try {
+            if (e.target.files) {
+                setIsImageUploading(true);
+                const pinataResult = await pinFileToIPFS(e.target.files[0]);
+                setForm({ ...form, imageUrl: pinataResult.data.IpfsHash });
+            }
+        } catch (error) {
+            console.log("Image Upload to Pinata Error", error);
+        } finally {
+            setIsImageUploading(false);
+        }
+    };
+
     return (
         <div className="container mx-auto my-10 pt-16">
             <div className="bg-white flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
@@ -111,3 +142,44 @@ const SellNft = () => {
                                 }
                             />
                         </div>
+
+                        <div className="flex-1 w-full flex flex-col">
+                            <div className="font-semibold text-[16px] leading-[22px] text-[#1e0c6e] mb-[10px]">
+                                NFT Image *
+                            </div>
+
+                            <input
+                                required
+                                onChange={onFileChange}
+                                type="file"
+                                className="py-[15px] sm:px-[25px] px-[15px] outline-none border-[3px] border-[#1e0c6e] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px] cursor-pointer"
+                            />
+                        </div>
+
+                        <div className="flex justify-center items-center mt-[40px]">
+                            {isImageUploading ? (
+                                <div className="bg-gray-500 px-16 rounded py-3">
+                                    Image Uploading...
+                                </div>
+                            ) : (
+                                <button
+                                    type="submit"
+                                    disabled={isListing}
+                                    className="bg-[#1e0c6e] px-16 rounded py-3 text-[#ffffff] text-[22px] rounded-[80px] transition-transform transform hover:scale-110"
+                                >
+                                    {isListing ? "Listing..." : "Submit"}
+                                </button>
+                            )}
+                        </div>
+                    </form>
+                ) : (
+                    <div className="mt-10 font-semibold align-center text-[20px] text-[#1e0c6e]">
+                        Connect your wallet to list an NFT.
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default SellNft;
