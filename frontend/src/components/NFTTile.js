3ec0a05915cdfcfab8b7a2738a068036ce81bd0d
Pratik Thorat <thorat.p@northeastern.edu>
2024-04-09 20:11:45 -0400

    Styling added to Base NFT Tile

--- frontend/src/components/NFTTile.js
+++ frontend/src/components/NFTTile.js
@@ -10,16 +10,16 @@ function NFTTile(data) {
 
   return (
     <Link to={newTo} className="relative z-0">
-      <div>
+      <div className="border-2 border-gray-800 ml-12 mt-5 mb-12 flex flex-col items-center rounded-lg md:w-72 shadow-2xl transition-transform duration-300 transform scale-100 hover:scale-110 inset-0">
         <img
           src={IPFSUrl}
           alt=""
           className="z-0 w-96 h-80 rounded-lg object-cover"
           crossOrigin="anonymous"
         />
-        <div>
-          <strong>{data.data.name}</strong>
-          <p>{data.data.description}</p>
+        <div className="text-white w-full p-2 bg-gradient-to-t from-[#454545] to-transparent rounded-lg pt-5 -mt-20 z-10">
+          <strong className="text-sm">{data.data.name}</strong>
+          <p className="display-inline">{data.data.description}</p>
         </div>
       </div>
     </Link>
